fd8bf122cb24368c0e41512e2a746f2d517e3e43
Jessica Nugent <jessicanugent@Jessicas-MacBook-Air.local>
2020-06-07 18:21:08 -0700

    Updated code to add API call

---
+++ Leaflet-Step-1/logic.js
@@ -0,0 +1,36 @@
+var myMap = L.map("map", {
+    center: [37.7749, -122.4194],
+    zoom: 13
+  });
+  
+
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
+
+var url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+
+d3.json(url, function(response) {
+
+    console.log(response);
+  
+    var heatArray = [];
+  
+    for (var i = 0; i < response.length; i++) {
+      var location = response[i].location;
+  
+      if (location) {
+        heatArray.push([location.coordinates[1], location.coordinates[0]]);
+      }
+    }
+  
+    var heat = L.heatLayer(heatArray, {
+      radius: 20,
+      blur: 35
+    }).addTo(myMap);
+  
+  });
+  
