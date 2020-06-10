a42a7d09eba28b7f0043440402d71dea018afa58
Jessica Nugent <jessicanugent@Jessicas-MacBook-Air.local>
2020-06-09 21:26:40 -0700

    Added markers to leaflet map

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -1,4 +1,4 @@
-var url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+var url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
 
 // Perform a GET request to the query URL
 d3.json(url, function(data) {
@@ -14,13 +14,45 @@ function createFeatures(earthquakeData) {
         "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
     };
 
+    function pointToFeature(feature, layer) {
+        var colorList = ["#99ff33", "#ccff99", "#ffcc66", "#ff9900", "#ff6600", "#ff3300"]
+
+        var color="";
+        if (feature.properties.mag > 5) {
+            color = colorList[5];
+        }
+        else if (feature.properties.mag > 4) {
+            color = colorList[4];
+        }
+        else if (feature.properties.mag > 3) {
+            color = colorList[3];
+        }
+        else if (feature.properties.mag > 2) {
+            color = colorList[2];
+        }
+        else if (feature.properties.mag > 1) {
+            color = colorList[1];
+        }
+        else {
+            color = colorList[0];
+        }
+
+        var markerStyle = {
+            radius: feature.properties.mag * 1000,
+            color: color,
+            fillOpacity: 0.75,
+        }; 
+
+    };
+
     // Create a GeoJSON layer containing the features array on the earthquakeData object
     // Run the onEachFeature function once for each piece of data in the array
     var earthquakes = L.geoJSON(earthquakeData, {
+        pointToFeature: pointToFeature,
         onEachFeature: onEachFeature
     });
-
-  // Sending our earthquakes layer to the createMap function
+  
+    // Sending our earthquakes layer to the createMap function
   createMap(earthquakes);
 };
 
@@ -32,8 +64,21 @@ function createMap(earthquakes) {
     accessToken: API_KEY
     });
 
+    // Create a baseMaps object to hold the streetmap layer
+    var baseMaps = {
+        "Street Map": streetmap
+    };
+
+    // Create an overlayMaps object to hold the earthquakes layer
+    var overlayMaps = {
+        "Earthquakes": earthquakes
+    };
+
     var myMap = L.map("map", {
         center: [37.7749, -122.4194],
-        zoom: 5
+        zoom: 5,
+        layers: [streetmap, earthquakes],
       });
+    
+    L.control.layers(baseMaps, overlayMaps).addTo(myMap);
 };
